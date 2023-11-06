508e68c58197dbec46b193a69644e755c4b314a7
Timur-Gus <timurka24478@gmail.ru>
2023-11-06 16:28:11 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
         isLeapYear(2023);
+        //Задача 2
+        isOS(2015,1);
+
     }
     public static void isLeapYear(int year){
         if ((year >= 1584 && year % 4 == 0 && year % 100 != 0) || (year >= 1584 && year % 4 == 0 && year % 400 == 0)) {
@@ -8,6 +14,20 @@ public class Main {
         }
         else {
             System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void isOS(int clientDeviceYear, int clientOS){
+        if (clientOS == 0 && clientDeviceYear > LocalDate.now().getYear()) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 0 && clientDeviceYear <= LocalDate.now().getYear()) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 1 && clientDeviceYear > LocalDate.now().getYear()) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        else if (clientOS == 1 && clientDeviceYear <= LocalDate.now().getYear()) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
     }
 
